26ab74bc73383dca70c4193079a98cbbc564645d
unknown <almi78@mail.ru>
2021-08-07 15:24:54 +0300

    Complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
+    let result = [];
+    let str = String(n);
+    let narr = str.split('');
+    result = (narr[0]=='-') ? (narr.splice(0,1), narr.reverse()): (narr.reverse());
+    return result.join('');
 }
